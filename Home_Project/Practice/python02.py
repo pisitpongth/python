80760db8024de16335a28fb2ddff1d2665d5ca47
pisitpongth <pisitpong242550@gmail.com>
2026-03-25 13:27:59 +0700

    format code in python02.py

--- Home_Project/Practice/python02.py
+++ Home_Project/Practice/python02.py
@@ -14,19 +14,27 @@ def divide(a, b):
     return a / b
 
 
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
+def main():
+    a = int(input("Enter the first number: "))
 
-print("[add, minus, multiply, divide]")
-operation = input("Enter the operation: ")
+    b = int(input("Enter the second number: "))
 
-if operation == "add":
-    result = plus(a, b)
-elif operation == "minus":
-    result = minus(a, b)
-elif operation == "multiply":
-    result = multiply(a, b)
-elif operation == "divide":
-    result = divide(a, b)
+    print("-" * 40)
+    print("add, minus, multiply, divide")
+    print("-" * 40)
+    operation = input("Enter the operation: ")
 
-print(f"The result of {a} {operation} {b} is: {result}")
+    if operation == "add":
+        result = plus(a, b)
+    elif operation == "minus":
+        result = minus(a, b)
+    elif operation == "multiply":
+        result = multiply(a, b)
+    elif operation == "divide":
+        result = divide(a, b)
+
+    print(f"The result is: {result}")
+
+
+if __name__ == "__main__":
+    main()
